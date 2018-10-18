68e03fe52a19f2f6b962e92535a5fedc225fa362
OneAndOnlySeabass <sebastiaan.koning@hetnet.nl>
2018-10-18 19:05:16 +0200

    Updates to GRU and RNN code
    Filled in part of the code with existing AstroNet code and added Apache license references

--- astronet/astronet/astro_rnn_model/astro_rnn_model.py
+++ astronet/astronet/astro_rnn_model/astro_rnn_model.py
@@ -1,7 +1,98 @@
+# Code written by Sebastiaan Koning (OneAndOnlySeabass), 
+# using parts of the TensorFlow AstroNet source code under the Apache License 2.0.
+#
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+
 """
 A recurrent model for classifying light curves.
 
-Architecture will be provided here once finished.
+See the base class (in astro_model.py) for a description of the general
+framework of AstroModel and its subclasses.
 
-Code will be written below.
+The architecture of this model is:
+
+
+                                     predictions
+                                          ^
+                                          |
+                                       logits
+                                          ^
+                                          |
+                                (fully connected layers)
+                                          ^
+                                          |
+                                   pre_logits_concat
+                                          ^
+                                          |
+                                    (concatenate)
+
+              ^                           ^                          ^
+              |                           |                          |
+   (recurrent blocks 1)         (recurrent blocks 2)    ...          |
+              ^                           ^                          |
+              |                           |                          |
+     time_series_feature_1     time_series_feature_2    ...     aux_features
+
+Code below is under construction.
 """
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+from astronet.astro_model import astro_model
+
+class AstroRNNModel(astro_model.AstroModel):
+    """A model for classifying light curves using a recurrent neural net."""
+    def __init__(self, features, labels, hparams, mode):
+        """Basic setup. The actual TensorFlow graph is constructed in build().
+
+    Args:
+      features: A dictionary containing "time_series_features" and
+          "aux_features", each of which is a dictionary of named input Tensors.
+          All features have dtype float32 and shape [batch_size, length].
+      labels: An int64 Tensor with shape [batch_size]. May be None if mode is
+          tf.estimator.ModeKeys.PREDICT.
+      hparams: A ConfigDict of hyperparameters for building the model.
+      mode: A tf.estimator.ModeKeys to specify whether the graph should be built
+          for training, evaluation or prediction.
+
+    Raises:
+      ValueError: If mode is invalid.
+    """
+    super(AstroRNNModel, self).__init__(features, labels, hparams, mode)
+    
+    def build_rnn_layers(self, inputs, hparams, scope="rnn"):
+    """ Builds recurrent layers and the embedding layer.
+    
+    Args:
+      inputs: A Tensor of shape [batch_size, length].
+      hparams: Object containing RNN hyperparameters.
+      scope: Name of the variable scope.
+
+    Returns:
+        A Tensor of shape [batch_size, output_size]
+    """
+        pass # TODO: set up architecture to build the RNN layers & Embedding layer
+        
+    def build_time_series_hidden_layers(self):
+    """Builds hidden layers for the time series features.
+
+    Inputs:
+      self.time_series_features
+
+    Outputs:
+      self.time_series_hidden_layers
+    """
+    time_series_hidden_layers = {}
+    for name, time_series in self.time_series_features.items():
+      time_series_hidden_layers[name] = self._build_rnn_layers(
+          inputs=time_series,
+          hparams=self.hparams.time_series_hidden[name],
+          scope=name + "_hidden")
+
+    self.time_series_hidden_layers = time_series_hidden_layers
--- astronet/astronet/astro_rnn_model/astro_rnn_model_test.py
+++ astronet/astronet/astro_rnn_model/astro_rnn_model_test.py
@@ -1,4 +1,47 @@
+# Code written by Sebastiaan Koning (OneAndOnlySeabass), 
+# using parts of the TensorFlow AstroNet source code under the Apache License 2.0.
+#
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+
 """
-Script for testing the Astronet RNN model
+Tests for astro_rnn_model.AstroRNNModel.
 Code will be provided in a later stadium
 """
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+
+from astronet.astro_rnn_model import astro_rnn_model
+from astronet.astro_rnn_model import configurations
+from astronet.ops import input_ops
+from astronet.ops import testing
+from astronet.util import configdict
+
+class AstroRNNModelTest(tf.test.TestCase):
+  
+  def assertShapeEquals(self, shape, tensor_or_array):
+    """Asserts that a Tensor or Numpy array has the expected shape.
+
+    Args:
+      shape: Numpy array or anything that can be converted to one.
+      tensor_or_array: tf.Tensor, tf.Variable, Numpy array or anything that can
+          be converted to one.
+    """
+    if isinstance(tensor_or_array, (np.ndarray, np.generic)):
+      self.assertAllEqual(shape, tensor_or_array.shape)
+    elif isinstance(tensor_or_array, (tf.Tensor, tf.Variable)):
+      self.assertAllEqual(shape, tensor_or_array.shape.as_list())
+    else:
+      raise TypeError("tensor_or_array must be a Tensor or Numpy ndarray")
+  
+  def testOneTimeSeriesFeature(self):
+    pass # TODO: build the testing structures
+  
+  def testTwoTimeSeriesFeatures(self):
+    pass # TODO: build the testing structures
